e6db31adb7bcc92f36665c5d5b409de3f497d302
alan@functionfirst.co.uk <alan@functionfirst.co.uk>
2015-01-19 21:10:04 +0000

    Storing search Keyword and Resource Type from the admin Catalogue page into session.

--- admin/resources.aspx.vb
+++ admin/resources.aspx.vb
@@ -14,4 +14,20 @@
             lblCount.Visible = False
         End If
     End Sub
+
+    Protected Sub rtbResourceName_Init(sender As Object, e As EventArgs) Handles rtbResourceName.Init
+        rtbResourceName.Text = Session("resourceKeywordSearch")
+    End Sub
+
+    Protected Sub rtbResourceName_TextChanged(sender As Object, e As EventArgs) Handles rtbResourceName.TextChanged
+        Session("resourceKeywordSearch") = rtbResourceName.Text
+    End Sub
+
+    Protected Sub rcbResourceType_Init(sender As Object, e As EventArgs) Handles rcbResourceType.Init
+        rcbResourceType.SelectedIndex = Session("resourceTypeSelected")
+    End Sub
+
+    Protected Sub rcbResourceType_SelectedIndexChanged(sender As Object, e As EventArgs) Handles rcbResourceType.SelectedIndexChanged
+        Session("resourceTypeSelected") = rcbResourceType.SelectedIndex
+    End Sub
 End Class
