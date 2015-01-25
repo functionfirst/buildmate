201f38ee8a578a97964ce6ea06a921a21889c600
alan@functionfirst.co.uk <alan@functionfirst.co.uk>
2015-01-25 15:31:13 +0000

    Moved membership assignment to variable into Page init. This was intermittently causing an error if the object not set as an instance.

--- common/Manager.master.vb
+++ common/Manager.master.vb
@@ -4,13 +4,16 @@ Imports System.Data.SqlClient
 Partial Class Manager
     Inherits System.Web.UI.MasterPage
 
-    Dim user As MembershipUser = Membership.GetUser
-    Dim userId As String = user.ProviderUserKey.ToString
+    Dim user As MembershipUser
+    Dim userId As String
     Dim subscriptionDate As Date
     Dim paypalPayerId As String
     Public user_email As String
 
     Protected Sub Page_Init(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Init
+        user = Membership.GetUser
+        userId = user.ProviderUserKey.ToString
+
         ' set userid
         Session("UserID") = userId
 
@@ -23,41 +26,6 @@ Partial Class Manager
             hlAddVariation.NavigateUrl = String.Format("~/project_details.aspx?pid={0}&action=add_variation", Request.QueryString("pid"))
             hlProjectStatus.NavigateUrl = String.Format("~/project_details.aspx?pid={0}", Request.QueryString("pid"))
         End If
-
-        '' check subscription date and calculate access permissions
-        'Dim dateDiff As TimeSpan = DateTime.Today - Convert.ToDateTime(subscriptionDate)
-        'Dim duration As String = dateDiff.TotalDays.ToString
-
-        'Select Case duration
-        '    Case Is >= 180
-        '        ' expired account. there has been no subscription for 6 months. prevent access to the system
-        '        Session("lockedAccount") = True
-        '        nav.Visible = False
-        '    Case Is >= 30
-        '        ' lock the account, the subscription is over 30 days out of date
-        '        Session("lockedAccount") = True
-        '        nav.Visible = False
-        '    Case Is >= 1
-        '        ' subscription has expired. check for paypal account
-        '        If IsNumeric(paypalPayerId) Then
-        '            ' user had a subscription but now expired so just limit their account
-        '            Session("limitedAccount") = True
-        '        Else
-        '            ' trial account so lock it
-        '            Session("lockedAccount") = True
-        '            nav.Visible = False
-        '        End If
-
-        '    Case Else
-        '        ' subscription is valid
-        '        Session("limitedAccount") = False
-        'End Select
-
-        '' account is locked, redirect to the subscription page if not already there
-        'Dim currentpage As String = System.IO.Path.GetFileName(System.Web.HttpContext.Current.Request.Url.AbsolutePath)
-        'If Session("lockedAccount") And currentpage <> "subscription.aspx" Then
-        '    Response.Redirect("~/subscription.aspx")
-        'End If
     End Sub
 
     Protected Sub getUserProfile()
